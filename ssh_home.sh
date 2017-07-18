af7a345c10e97f0f9b11294493793dd464da56dc
Le Coz Gael <lecoz@NBMAC0011.local>
2017-07-18 12:02:26 +0200

    - Added proxy to ssh tunnel

--- ssh_home.sh
+++ ssh_home.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ssh -p 22222 -L 9091:localhost:9091 glecoz@gaelpierre.internet-box.ch
+while true; do
+	ssh -p 22222 -L 9091:localhost:9091 -D 65000 glecoz@gaelpierre.internet-box.ch
+done
